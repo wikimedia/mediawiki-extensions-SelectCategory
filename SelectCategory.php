cd61834fbfa3e122f721b52493b896ba57c01114
Jack Phoenix <ashley@uncyclomedia.co>
2025-08-07 19:48:43 +0300

    Move PHP source files into an includes/ subdirectory so that this repo may run phan one day
    Also updated the required MW version in the error message emitted by the legacy PHP entry point to be consistent with the one defined in extension.json.
    
    Change-Id: Ie1679c11a1862811bb423364e5248435a8022f3b

--- SelectCategory.php
+++ SelectCategory.php
@@ -10,5 +10,5 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	);
 	return;
 } else {
-	die( 'This version of the SelectCategory extension requires MediaWiki 1.29+' );
+	die( 'This version of the SelectCategory extension requires MediaWiki 1.43+' );
 }
